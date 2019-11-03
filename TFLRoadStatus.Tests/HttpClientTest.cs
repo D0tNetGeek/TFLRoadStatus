dedace3ae650085b63196732619da4543f98d6ed
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-03 21:56:55 +0000

    Completed HttpClient tests

--- TFLRoadStatus.Tests/HttpClientTest.cs
+++ TFLRoadStatus.Tests/HttpClientTest.cs
@@ -1,8 +1,12 @@
 ﻿using Moq;
+using Newtonsoft.Json;
+using System.Linq;
 using System.Net.Http;
 using TFLRoadStatus.API;
 using TFLRoadStatus.API.Interfaces;
+using TFLRoadStatus.API.Models;
 using TFLRoadStatus.Stubs;
+using Xunit;
 
 namespace TFLRoadStatus.Tests
 {
@@ -11,13 +15,70 @@ namespace TFLRoadStatus.Tests
         private readonly Mock<IConfig> _configMock;
         private readonly HttpClient _httpClient;
         private readonly IRestClient _restClient;
-        private string road;
+        private string _road;
 
         public HttpClientTest()
         {
             _configMock = new Mock<IConfig>();
             _httpClient = new HttpClient(new DelegatingHandlerStub());
             _restClient = new RestClient(_configMock.Object, _httpClient);
+        }
+
+        [Fact]
+        public void When_Execute_Request_With_Correct_Url()
+        {
+            _road = "A2";
+            var expectedRoad = "A2";
+            var expectedSeverity = "Good";
+
+            SetupConfigUri("http://localhost/HttpStatus200");
+
+            var roadStatus = GetRoadStatus<SeverityStatus[]>().First();
+
+            Assert.NotNull(roadStatus);
+
+            Assert.Equal(expectedRoad, roadStatus.displayName);
+            Assert.Equal(expectedSeverity, roadStatus.statusSeverity);
+        }
+
+        [Fact]
+        public void When_Execute_Request_With_Wrong_Url()
+        {
+            _road = "A2";
+
+            SetupConfigUri("http://localhost/HttpStatus400");
+
+            Assert.Throws<HttpRequestException>(() => _restClient.Get(_road));
+        }
+
+        [Fact]
+        public void When_Execute_Request_With_Invalid_Road_Then_Returns_Json()
+        {
+            _road = "A223";
+
+            var expectedHttpStatus = "NotFound";
+            var expectedHttpStatusCode = "404";
+
+            SetupConfigUri("http://localhost/HttpStatus404");
+
+            var roadStatus = GetRoadStatus<NotFoundStatus>();
+
+            Assert.NotNull(roadStatus);
+
+            Assert.Equal(expectedHttpStatus, roadStatus.httpStatus);
+            Assert.Equal(expectedHttpStatusCode, roadStatus.httpStatusCode);
+        }
+
+        private T GetRoadStatus<T>()
+        {
+            var result = _restClient.Get(_road);
+
+            return JsonConvert.DeserializeObject<T>(result);
+        }
+
+        private void SetupConfigUri(string uri)
+        {
+            _configMock.Setup(x => x.Url).Returns(() => uri);
         }
     }
 }
