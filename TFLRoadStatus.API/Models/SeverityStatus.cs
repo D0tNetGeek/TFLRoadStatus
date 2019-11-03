bb5be66b52fb91707fcfd01f86b1372f66ca7aef
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-03 03:16:13 +0000

    Added properties to SeverityStatus model

--- TFLRoadStatus.API/Models/SeverityStatus.cs
+++ TFLRoadStatus.API/Models/SeverityStatus.cs
@@ -5,7 +5,7 @@
         public string id { get; set; }
         public string displayName { get; set; }
         public string statusSeverity { get; set; }
-        public string statuSeverityDescription { get; set; }
+        public string statusSeverityDescription { get; set; }
         public string bounds { get; set; }
         public string envelope { get; set; }
         public string url { get; set; }
