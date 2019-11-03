a1b4bb5cdb5d90c8f6d0dc10cb499a5588231666
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-03 03:23:28 +0000

    Added http client test

---
+++ TFLRoadStatus.Tests/HttpClientTest.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using System.Net.Http;
+using TFLRoadStatus.API;
+using TFLRoadStatus.API.Interfaces;
+using TFLRoadStatus.Stubs;
+
+namespace TFLRoadStatus.Tests
+{
+    public class HttpClientTest
+    {
+        private readonly Mock<IConfig> _configMock;
+        private readonly HttpClient _httpClient;
+        private readonly IRestClient _restClient;
+        private string road;
+
+        public HttpClientTest()
+        {
+            _configMock = new Mock<IConfig>();
+            _httpClient = new HttpClient(new DelegatingHandlerStub());
+            _restClient = new RestClient(_configMock.Object, _httpClient);
+        }
+    }
+}
