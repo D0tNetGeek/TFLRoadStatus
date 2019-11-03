7e198704330977a21d4fd2f55c99cf6d09dc40c0
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-03 03:17:27 +0000

    Completed RestClient  functionality

--- TFLRoadStatus.API/RestClient.cs
+++ TFLRoadStatus.API/RestClient.cs
@@ -15,11 +15,36 @@ namespace TFLRoadStatus.API
             _httpClient = httpClient;
         }
 
-        public HttpStatusCode StatusCode => throw new System.NotImplementedException();
+        public HttpStatusCode StatusCode { get; private set; }
 
         public string Get(string road)
         {
-            throw new System.NotImplementedException();
+            string content = string.Empty;
+
+            using (_httpClient)
+            {
+                var requestUrl = CreateUrl(road);
+
+                var httpContent = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                HttpResponseMessage response = _httpClient.SendAsync(httpContent).Result;
+
+                StatusCode = response.StatusCode;
+
+                if(response.StatusCode !=HttpStatusCode.OK && response.StatusCode != HttpStatusCode.NotFound)
+                {
+                    throw new HttpRequestException($"Error request http status code {response.StatusCode}");
+                }
+
+                content = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return content;
+        }
+
+        private string CreateUrl(string road)
+        {
+            return string.Format(_config.Url + "{0}?app_id{1}&app_key={2}", road, _config.AppId, _config.AppKey);
         }
     }
 }
