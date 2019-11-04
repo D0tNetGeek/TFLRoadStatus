1006377a58e00eb321fd805ae4fa48c35a1c88ef
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-04 02:22:15 +0000

    Added DI container

---
+++ TFLRoadStatus.Console/RoadStatusContainer.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using TFLRoadStatus.API;
+
+namespace TFLRoadStatus.Console
+{
+    public static class RoadStatusContainer
+    {
+        public static IContainer Container { get; set; }
+
+        static RoadStatusContainer()
+        {
+            Container = Build();
+        }
+
+        private static IContainer Build()
+        {
+            var container = new ContainerBuilder();
+
+            container.RegisterModule(new TFLRoadStatusAPIContainer());
+
+            return container.Build();
+        }
+    }
+}
