0d44241b6b3f85c0d0e818f2e336824ccfda1224
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-04 02:53:14 +0000

    Implemented functionality for finding road status

--- TFLRoadStatus.Console/Program.cs
+++ TFLRoadStatus.Console/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace TFLRoadStatus.Console
+﻿using Autofac;
+using System.Linq;
+using TFLRoadStatus.API.Interfaces;
+
+namespace TFLRoadStatus.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            var roadStatusApp = RoadStatusContainer.Container.Resolve<IRoadStatusValidator>();
+
+            string parameter;
+
+            if ((parameter = ParseArgs(args)) != null)
+                return roadStatusApp.GetCurrentRoadStatus(parameter);
+
+            return -1;
+        }
+
+        private static string ParseArgs(string[] args)
+        {
+            if (args != null && args.Length == 1)
+                return args.First();
+
+            PrintHelp();
+
+            return null;
+        }
+
+        private static void PrintHelp()
+        {
+            var print = RoadStatusContainer.Container.Resolve<IPrint>();
+
+            print.AddHelp().PrintStatus();
         }
     }
 }
