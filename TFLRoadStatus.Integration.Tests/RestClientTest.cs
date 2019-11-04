0224505ec38be16242269959af5f521799697c5e
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-04 02:03:09 +0000

    Added integration test for RestClient

---
+++ TFLRoadStatus.Integration.Tests/RestClientTest.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Newtonsoft.Json;
+using System.Linq;
+using TFLRoadStatus.API.Interfaces;
+using TFLRoadStatus.API.Models;
+using Xunit;
+
+namespace TFLRoadStatus.Integration.Tests
+{
+    public class RestClientTest
+    {
+        private readonly IRestClient _restClient;
+        private string _road;
+
+        public RestClientTest()
+        {
+            _restClient = TFLRoadStatusContainer.Container.Resolve<IRestClient>();
+        }
+
+        [Fact]
+        public void When_Request_Is_Executed_With_Correct_Road_Then_Returns_Json()
+        {
+            _road = "A2";
+            var expectedRoad = "A2";
+
+            var result = _restClient.Get(_road);
+
+            var road = JsonConvert.DeserializeObject<SeverityStatus[]>(result).First();
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedRoad, road.displayName);
+        }
+    }
+}
