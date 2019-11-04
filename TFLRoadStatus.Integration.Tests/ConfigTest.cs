1ba3bf2b0e8154103047c8950b857575d4130aa0
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-04 03:28:22 +0000

    Removed appId and appKey

--- TFLRoadStatus.Integration.Tests/ConfigTest.cs
+++ TFLRoadStatus.Integration.Tests/ConfigTest.cs
@@ -17,8 +17,8 @@ namespace TFLRoadStatus.Integration.Tests
         public void When_Config_Is_Instantiated_Then_Correct_Values_Are_Found()
         {
             var expectedUrl = "http://api.tfl.gov.uk/Road/";
-            var expectedAppId = "bc00d3e7";
-            var expectedAppKey = "5012b3bec3afacd56b713cc06d26a52d	";
+            var expectedAppId = "";
+            var expectedAppKey = "";
 
             Assert.NotNull(_config);
             Assert.Equal(expectedUrl, _config.Url);
