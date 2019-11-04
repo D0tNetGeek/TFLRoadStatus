27ee55e4a782891d0f346724f4bc6f469e9cd883
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-04 01:58:12 +0000

    Added Integration test for Config

---
+++ TFLRoadStatus.Integration.Tests/ConfigTest.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using TFLRoadStatus.API.Interfaces;
+using Xunit;
+
+namespace TFLRoadStatus.Integration.Tests
+{
+    public class ConfigTest
+    {
+        private readonly IConfig _config;
+
+        public ConfigTest()
+        {
+            _config = TFLRoadStatusContainer.Container.Resolve<IConfig>();
+        }
+
+        [Fact]
+        public void When_Config_Is_Instantiated_Then_Correct_Values_Are_Found()
+        {
+            var expectedUrl = "http://api.tfl.gov.uk/Road/";
+            var expectedAppId = "bc00d3e7";
+            var expectedAppKey = "5012b3bec3afacd56b713cc06d26a52d	";
+
+            Assert.NotNull(_config);
+            Assert.Equal(expectedUrl, _config.Url);
+            Assert.Equal(expectedAppId, _config.AppId);
+            Assert.Equal(expectedAppKey, _config.AppKey);
+        }
+    }
+}
---
+++ TFLRoadStatus.Integration.Tests/TFLRoadStatusContainer.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using TFLRoadStatus.API;
+
+namespace TFLRoadStatus.Integration.Tests
+{
+    public static class TFLRoadStatusContainer
+    {
+        public static IContainer Container { get; set; }
+
+        static TFLRoadStatusContainer()
+        {
+            Container = Build();
+        }
+
+        private static Autofac.IContainer Build()
+        {
+            var container = new ContainerBuilder();
+
+            container.RegisterModule(new TFLRoadStatusAPIContainer());
+
+            return container.Build();
+        }
+    }
+}
