61cecd1e4740049084bf465ecd56c9117d222e71
Vinod Malvi <vinod.malvi@yahoo.co.uk>
2019-11-04 01:57:50 +0000

    Added DI container

---
+++ TFLRoadStatus.API/TFLRoadStatusAPIContainer.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using System.Net.Http;
+using TFLRoadStatus.API.Interfaces;
+
+namespace TFLRoadStatus.API
+{
+    public class TFLRoadStatusAPIContainer : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => new Config()).As<IConfig>();
+            builder.Register(c => new RestClient(c.Resolve<IConfig>(), new HttpClient())).As<IRestClient>();
+            builder.Register(c => new Print()).As<IPrint>();
+            builder.Register(c => new RoadStatusValidator(c.Resolve<IRestClient>(), c.Resolve<IPrint>())).As<IRoadStatusValidator>();
+        }
+    }
+}
